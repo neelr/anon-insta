ae6f47ce5cb1e8f55d956b222d67e98e344dd7b1
Neel Redkar <neel.redkar@outlook.com>
2020-05-29 04:33:33 -0700

    add puppeteer screenshots

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const { IgApiClient } = require("instagram-private-api");
-const html2image = require("node-html-to-image");
 const fs = require("fs");
 const Airtable = require("airtable");
 const words = require("./utils/words");
 var crypto = require("crypto");
 const express = require("express");
+const puppeteer = require("puppeteer");
 const next = require("next");
 const axios = require("axios");
-const qs = require("querystring")
+const qs = require("querystring");
 const base = new Airtable({ apiKey: process.env.AIRTABLE }).base(
     process.env.BASE
 );
@@ -23,20 +23,41 @@ const handle = server.getRequestHandler();
 const MAX_ID = "recwllrvK6qSCN46G";
 const ig = new IgApiClient();
 
+var loadHTML = async (name, number, body) => {
+    const browser = await puppeteer.launch({
+        args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 800, height: 500 });
+    await page.setContent(
+        html
+            .toString()
+            .replace("{{body}}", body.replace("<", "&lt").replace(">", "&rt"))
+            .replace("{{name}}", name.replace("<", "&lt").replace(">", "&rt"))
+            .replace("{{postNumber}}", number)
+    );
+    let imgBuffer = await page.screenshot({ type: "jpeg", quality: 100 });
+    await browser.close();
+    return imgBuffer;
+};
+
 var login = async () => {
     ig.state.generateDevice(process.env.IG_USERNAME);
     ig.state.proxyUrl = process.env.IG_PROXY;
     await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
 };
 
-var checkCaptcha = (captcha) => {
+var checkCaptcha = captcha => {
     return new Promise((res, rej) => {
         axios
-            .post("https://www.google.com/recaptcha/api/siteverify", qs.stringify({
-                secret: process.env.CAPTCHA,
-                response: captcha,
-            }))
-            .then((d) => {
+            .post(
+                "https://www.google.com/recaptcha/api/siteverify",
+                qs.stringify({
+                    secret: process.env.CAPTCHA,
+                    response: captcha
+                })
+            )
+            .then(d => {
                 if (d.data.success) {
                     res();
                     return;
@@ -64,69 +85,57 @@ server.prepare().then(() => {
                     .toString();
                 base("IP Banlist")
                     .select({
-                        filterByFormula: `{IP}='${ipHash}'`,
+                        filterByFormula: `{IP}='${ipHash}'`
                     })
                     .firstPage((err, records) => {
                         if (records.length == 0) {
-                            base("Current Number").find(
-                                MAX_ID,
-                                async (err, record) => {
-                                    await login();
-                                    let image = await html2image({
-                                        html: html
-                                            .toString()
-                                            .replace("{{body}}", req.body.body)
-                                            .replace("{{name}}", req.body.name)
-                                            .replace(
-                                                "{{number}}",
-                                                record.get("MAX") + 1
-                                            )
-                                            .replace("<", "&lt")
-                                            .replace(">", "&rt"),
-                                    });
-                                    let pngBuffer = await pngToJpeg({
-                                        quality: 100,
-                                    })(image);
-                                    let result = await ig.publish.photo({
+                            base("Current Number").find(MAX_ID, async (err, record) => {
+                                await login();
+                                let pngBuffer = await loadHTML(
+                                    req.body.name,
+                                    record.get("MAX") + 1,
+                                    req.body.body
+                                );
+                                let result = await ig.publish
+                                    .photo({
                                         file: pngBuffer,
-                                        caption: "my caption",
-                                    });
-                                    base("Posts").create([
-                                        {
-                                            fields: {
-                                                PersonTag: req.body.name,
-                                                Note: req.body.body,
-                                                IP: ipHash,
-                                                "Post Number":
-                                                    record.get("MAX") + 1,
-                                                MediaID: result.media.id,
-                                            },
-                                        },
-                                    ]);
-                                    base("Current Number").update([
-                                        {
-                                            id: MAX_ID,
-                                            fields: {
-                                                MAX: record.get("MAX") + 1,
-                                            },
-                                        },
-                                    ]);
-                                    res.sendStatus(200);
-                                }
-                            );
+                                        caption: `${req.body.body}\n - ${req.body.name}`
+                                    })
+                                    .catch(e => console.log(e));
+                                base("Posts").create([
+                                    {
+                                        fields: {
+                                            PersonTag: req.body.name,
+                                            Note: req.body.body,
+                                            IP: ipHash,
+                                            "Post Number": record.get("MAX") + 1,
+                                            MediaID: result.media.id
+                                        }
+                                    }
+                                ]);
+                                base("Current Number").update([
+                                    {
+                                        id: MAX_ID,
+                                        fields: {
+                                            MAX: record.get("MAX") + 1
+                                        }
+                                    }
+                                ]);
+                                res.sendStatus(200);
+                            });
                         } else {
                             res.sendStatus(401);
                         }
                     });
             })
-            .catch((d) => {
+            .catch(d => {
                 res.sendStatus(401);
             });
     });
 
     app.post("/api/comment", (req, res) => {
         checkCaptcha(req.body.captcha)
-            .then((d) => {
+            .then(d => {
                 if (!(req.body.body && req.body.postNumber)) {
                     res.sendStatus(500);
                     return;
@@ -138,47 +147,38 @@ server.prepare().then(() => {
                     .toString();
                 base("IP Banlist")
                     .select({
-                        filterByFormula: `{IP}='${ipHash}'`,
+                        filterByFormula: `{IP}='${ipHash}'`
                     })
                     .firstPage((err, records) => {
                         if (records.length == 0) {
                             base("Posts")
                                 .select({
-                                    filterByFormula: `{Post Number}='${req.body.postNumber}'`,
+                                    filterByFormula: `{Post Number}='${req.body.postNumber}'`
                                 })
-                                .eachPage((records) => {
+                                .eachPage(records => {
                                     if (records.length == 0) {
                                         res.sendStatus(404);
                                         return;
                                     }
-                                    records.forEach(async (record) => {
+                                    records.forEach(async record => {
                                         let prefixHash = crypto
                                             .createHash("sha256")
-                                            .update(
-                                                `${req.ip}-${record.get(
-                                                    "Post Number"
-                                                )}`,
-                                                "utf8"
-                                            )
+                                            .update(`${req.ip}-${record.get("Post Number")}`, "utf8")
                                             .digest("hex")
                                             .toString();
                                         let prefix;
                                         if (record.get("IP") == ipHash) {
-                                            prefix = "OP";
+                                            prefix = "🔴 OP";
                                         } else {
                                             prefix = `${
                                                 words.adjectives[
-                                                parseInt(
-                                                    prefixHash.slice(0, 32),
-                                                    16
-                                                ) % words.adjectives.length
+                                                parseInt(prefixHash.slice(0, 32), 16) %
+                                                words.adjectives.length
                                                 ]
                                                 } ${
                                                 words.animals[
-                                                parseInt(
-                                                    prefixHash.slice(32),
-                                                    16
-                                                ) % words.animals.length
+                                                parseInt(prefixHash.slice(32), 16) %
+                                                words.animals.length
                                                 ]
                                                 }`;
                                         }
@@ -186,7 +186,7 @@ server.prepare().then(() => {
                                         await login();
                                         ig.media.comment({
                                             mediaId: record.get("MediaID"),
-                                            text: `${prefix}: ${req.body.body}`,
+                                            text: `${prefix}: ${req.body.body}`
                                         });
                                         res.sendStatus(200);
                                     });
@@ -196,7 +196,7 @@ server.prepare().then(() => {
                         }
                     });
             })
-            .catch((d) => {
+            .catch(d => {
                 res.sendStatus(401);
             });
     });
